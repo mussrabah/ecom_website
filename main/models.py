ff569e9e74c412e6aa88a8d7428b448820928045
Mustapha Rabah <mustrbh@gmail.com>
2024-05-10 19:05:49 +0100

    Created and structured the ecom website project

---
+++ main/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
---
+++ main/views.py
@@ -0,0 +1,10 @@
+# views.py
+from django.shortcuts import render
+from .models import Product
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+# Create your views here.
